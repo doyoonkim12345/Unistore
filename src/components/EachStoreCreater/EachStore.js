de33688ca2ee471b42ddbea4818ff5babc0f62a1
doyoonkim12345 <kdyem0628@gmail.com>
2021-08-29 01:01:33 +0900

    Fix styles

--- src/components/EachStoreCreater/EachStore.js
+++ src/components/EachStoreCreater/EachStore.js
@@ -1,28 +1,35 @@
 import React from 'react';
-import { DataContainer, EachBackGround, EachStoreBack } from '../style';
+import { StoreImg, EachStoreBack, RtmEvents, Contents, Buttons} from '../style';
 
 const storeTestData = {
-    logo:'란탕수육 이대점',
-    sTitle:'맥주1+!',
-    mainTitle:'9시부터 10시까지만',
-    max:1,
-    explain:'항상 대학생 10% 할인',
-    imgUrl:'https://pelicana.co.kr/resources/images/menu/original_menu01_200529.png',
-    color:'skyblue'
+    storeName:'란탕수육 이대점',
+    realTime:'맥주1+1',
+    startTime: 9,
+    endTime: 10,
+    always:'항상 대학생 10% 할인',
+    imgUrl:'https://www.eguljak.com/upload/product/3696654736_oTZCrFQ3_20210726032314.jpg',
+    color:'skyblue',
+    tel:'tel:010-0000-0000',
+    map:'https://map.kakao.com/link/to/카카오판교오피스,37.402056,127.108212'
 }
 
 export default function EachStore({}){
     return(
             <EachStoreBack color={storeTestData.color}>
-                <DataContainer>
-                    <p className='logoStyle'>{storeTestData.logo}</p>
-                    <p className='sTitleStyle'>{storeTestData.sTitle}</p>
-                    <span className='maxStyle'>{storeTestData.max ? '최대 ' : ''}</span>
-                    <span className='mainTitleStyle'>{storeTestData.mainTitle}</span>
-                    <p className='explainStyle'>{storeTestData.explain}</p>
-                </DataContainer>
-                <EachBackGround imgUrl={storeTestData.imgUrl}></EachBackGround>
-                <a href={'tel:010-0000-0000'}>전화</a><a href={"https://map.kakao.com/link/to/카카오판교오피스,37.402056,127.108212"}>길찾기</a>
+                <StoreImg imgUrl={storeTestData.imgUrl}></StoreImg>
+                <Contents>
+                <span className="Name">{storeTestData.storeName}</span>
+                <RtmEvents>
+                <span className="realtime">{storeTestData.realTime}</span>
+                <span className="time">{`${storeTestData.startTime}시부터 ${storeTestData.endTime}까지만`}</span>
+                </RtmEvents>
+                <span className="Always">{storeTestData.always}</span>
+                <Buttons>
+                <a href={storeTestData.tel}>전화</a>
+                <span>|</span>
+                <a href={storeTestData.map}>길찾기</a>
+                </Buttons>
+                </Contents>
             </EachStoreBack>
     );
 }
--- src/components/style.js
+++ src/components/style.js
@@ -7,8 +7,6 @@ export const ListContainer = styled.div`
     display:flex;
     justify-content : center;
     flex-wrap: wrap;
-    align-items: center;
-
 `
 
 export const EachListContainer = styled.div``
@@ -19,61 +17,74 @@ export const EachListContainer = styled.div``
 
 
 export const EachStoreBack = styled.div`
-    order:-10;
-    width : 24rem;
-    height : 12rem;
+    width : 20rem;
+    height : 21.875rem;
     margin-top:1rem;
     margin-left:0.5rem;
     margin-right:0.5rem;
-    border-radius: 1em;
-    transition: 400ms;
-    align-items: center;
-    display:flex;
-    background-color: ${props => props.color || "red"};
+    border-radius: 10px;
+    overflow: hidden;
+    
+    flex-direction: column;
+    justify-content: center;
+    background-color: ${props => props.color || "yellow"};
     //만약 백그라운드 색을 보내지 않았다면 기본값 빨간색 props. 다음에 갖고 오고 싶은 인자 이름 적기
-    :active{
-        width:30rem;
-        height:15rem;
-    }
 `;
 
-export const DataContainer = styled.div`
-    width : 20rem;
-    margin-left: 1.25rem;
-    text-align:left;
-    align-content: center;
-    position: absolute;
-    display: inline;
-    .logoStyle{
-        font-size:18px;
-        font-weight: 600;
-    }
-    .sTitleStyle{
-        font-size:18px;
-        font-weight: 500;
-        margin-bottom:-0.25rem;
-        margin-top:-0.5rem;
-    }
-    .maxStyle{
-        font-size:10px;
-        writing-mode: vertical-rl;
-        font-weight: 900;
-    }
-    .mainTitleStyle{
-        font-size:30px;
-        font-weight: 900;
-    }
-    .explainStyle{
-        font-size:12px;
-    }
-    p { width:65%;}
-`;
-
-export const EachBackGround= styled.div`
+export const StoreImg= styled.div`
     width:100%;
-    height:100%;
+    height:11.25rem;
+    border-radius: 10px 10px 0 0;
     background-image: url(${props=> props.imgUrl});
-    background-position: 130% center;
-    background-size: 70%;
+    background-size: cover;
     background-repeat: no-repeat;
+`;
+
+export const RtmEvents=styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-left: 12px;
+    padding-right:12px;
+    .realtime{
+        font-size: 30px;
+        font-weight : bold;
+    }
+    .time{
+        font-weight:bold;
+    }
+`;
+
+export const Contents=styled.div`
+    display:flex;
+    flex-direction: column;
+    width:100;
+    height:10.625rem;
+    .Name {
+        font-size: 16px;
+        margin: 0.5rem;
+    }
+    .Always{
+        text-align: start;
+        padding-left: 12px;
+        margin-top: 10px;
+        font-size: 18px;
+    }
+
+`;
+
+export const Buttons=styled.div`
+    width: 100%;
+    flex-direction:row;
+    display:flex;
+    justify-content: space-around;
+    margin-top: 1.2rem;
+    a{
+        color: black;
+        width: 10rem;
+        text-align: center;
+        margin-bottom: 4px;
+        text-decoration: none;
+        font-weight:600;
+    }
 `;
