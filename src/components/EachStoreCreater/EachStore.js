3d540f274d13a2dfa08edaf93078e3c5cc6dc272
doyoonkim12345 <kdyem0628@gmail.com>
2021-08-28 23:00:12 +0900

    Add tel link and map

--- src/components/EachStoreCreater/EachStore.js
+++ src/components/EachStoreCreater/EachStore.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { DataContainer, EachBackGround, EachStoreBack } from '../style';
 
-/*const storeTestData = {
-    logo:'치킨플러스',
-    sTitle:'신메뉴 출시 기념',
-    mainTitle:'5천원 할인',
+const storeTestData = {
+    logo:'란탕수육 이대점',
+    sTitle:'맥주1+!',
+    mainTitle:'9시부터 10시까지만',
     max:1,
-    explain:'8월 4일~8월 31일 닭볶이 5천원',
+    explain:'항상 대학생 10% 할인',
     imgUrl:'https://pelicana.co.kr/resources/images/menu/original_menu01_200529.png',
     color:'skyblue'
-}*/
+}
 
-export default function EachStore({storeTestData}){
+export default function EachStore({}){
     return(
             <EachStoreBack color={storeTestData.color}>
                 <DataContainer>
@@ -22,6 +22,7 @@ export default function EachStore({storeTestData}){
                     <p className='explainStyle'>{storeTestData.explain}</p>
                 </DataContainer>
                 <EachBackGround imgUrl={storeTestData.imgUrl}></EachBackGround>
+                <a href={'tel:010-0000-0000'}>전화</a><a href={"https://map.kakao.com/link/to/카카오판교오피스,37.402056,127.108212"}>길찾기</a>
             </EachStoreBack>
     );
 }
